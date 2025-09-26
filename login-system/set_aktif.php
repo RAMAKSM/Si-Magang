a7bd292d59520f70f7f960070d4dcfe3265a6158
RAMA ANTONIUS <144994212+RAMAKSM@users.noreply.github.com>
2025-09-27 04:26:38 +0700

    Update set_aktif.php

--- login-system/set_aktif.php
+++ login-system/set_aktif.php
@@ -1,5 +1,24 @@
 <?php
+session_start();
 include "db.php";
-$id = $_GET['id'];
-mysqli_query($conn, "UPDATE mahasiswa1 SET status='aktif' WHERE id='$id'");
-header("Location: admin_dashboard.php");
+
+if (isset($_GET['id'])) {
+  $id = mysqli_real_escape_string($conn, $_GET['id']); // aman dari SQL Injection
+
+  $query = "UPDATE mahasiswa1 SET status='aktif' WHERE id='$id'";
+  if (mysqli_query($conn, $query)) {
+    $_SESSION['pesan'] = "✅ Status mahasiswa berhasil diubah menjadi <b>Diterima</b>";
+    $_SESSION['status'] = "success"; // warna hijau
+  } else {
+    $_SESSION['pesan'] = "❌ Gagal mengubah status!";
+    $_SESSION['status'] = "danger"; // warna merah
+  }
+
+  header("Location: data_admin.php");
+  exit;
+} else {
+  $_SESSION['pesan'] = "❌ ID tidak ditemukan!";
+  $_SESSION['status'] = "danger";
+  header("Location: data_admin.php");
+  exit;
+}
